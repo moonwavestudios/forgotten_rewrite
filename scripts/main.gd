3888ef7cbbf6a56790f0997b4bd97362013c3e91
Virus01Official <virusidkoff@gmail.com>
2026-04-21 13:27:54 +0200

    fixed killer being assigned to everyone

--- scripts/main.gd
+++ scripts/main.gd
@@ -72,8 +72,6 @@ func start_round():
 		player.get_node('player_ui').get_node('SpectatorStuff').visible = false
 		player.get_node('player_ui').get_node('GameStuff').visible = true
 		player.in_round = true
-		
-		most_malicious_player.is_Killer = true
 			
 		
 	if most_malicious_player != null:
