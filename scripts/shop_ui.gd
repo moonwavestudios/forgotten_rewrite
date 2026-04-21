211df2a87916dd488b0bf1fb5099f911c2e5f1e2
Virus01Official <virusidkoff@gmail.com>
2026-04-21 14:06:01 +0200

    still working on the shop HELP ME, SOMEBODY SAVE ME

--- scripts/shop_ui.gd
+++ scripts/shop_ui.gd
@@ -4,3 +4,15 @@ func _on_killers_button_pressed() -> void:
 	$Killers.visible = true
 	$Survivors.visible = false
 	$Emotes.visible = false
+
+
+func _on_survivors_button_pressed() -> void:
+	$Killers.visible = false
+	$Survivors.visible = true
+	$Emotes.visible = false
+
+
+func _on_emotes_button_pressed() -> void:
+	$Killers.visible = false
+	$Survivors.visible = false
+	$Emotes.visible = true
