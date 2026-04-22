eb09a2aac013daf5730cf4ee9ef942e8e9e107f3
Virus01Official <virusidkoff@gmail.com>
2026-04-22 13:17:58 +0200

    shop works

---
+++ scripts/server_settings.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var use_stamina = true
--- scripts/shop.gd
+++ scripts/shop.gd
@@ -1,33 +1,51 @@
 extends Panel
 
 func _ready() -> void:
-	#for item in $Items/Killers/GridContainer.get_children():
-		#item.get_node('Button').pressed.connect(select_item.bind(item))
-	if CharData.killers.is_empty():
+	if CharData.killers.is_empty() and CharData.survivors.is_empty():
 		await CharData.data_loaded
 	
 	_populate_killers()
+	_populate_survivors()
+	_populate_emotes()
 
 func _populate_killers() -> void:
 	var grid = $Items/Killers/GridContainer
-	
-	for child in grid.get_children():
-		child.queue_free()
+	_clear_grid(grid)
 	
 	for killer_id in CharData.killers:
 		var killer = CharData.killers[killer_id]
-		var item = _create_killer_item(killer)
+		var item = _create_char_item(killer)
 		grid.add_child(item)
-		item.get_node("Button").pressed.connect(select_item.bind(item))
+		item.get_node("Button").pressed.connect(select_char_item.bind(item))
 
-func _create_killer_item(killer: Dictionary) -> Control:
+func _populate_survivors() -> void:
+	var grid = $Items/Survivors/GridContainer
+	_clear_grid(grid)
+	
+	for survivor_id in CharData.survivors:
+		var survivor = CharData.survivors[survivor_id]
+		var item = _create_char_item(survivor)
+		grid.add_child(item)
+		item.get_node("Button").pressed.connect(select_char_item.bind(item))
+
+func _populate_emotes() -> void:
+	var grid = $Items/Emotes/GridContainer
+	_clear_grid(grid)
+	
+	for emote_name in EmoteData.Emotes:
+		var emote = EmoteData.Emotes[emote_name]
+		var item = _create_emote_item(emote_name, emote)
+		grid.add_child(item)
+		item.get_node("Button").pressed.connect(select_emote_item.bind(item))
+
+func _create_char_item(char_data: Dictionary) -> Control:
 	var item_scene = preload("res://UI/stuff/ShopButton.tscn")
 	var item = item_scene.instantiate()
 	
-	item.get_node("ItemName").text = killer.get("display_name", killer["id"])
-	item.get_node("Price").text = "$ %d" % killer.get("stats", {}).get("price", 0)
+	item.get_node("ItemName").text = char_data.get("display_name", char_data.get("id", "???"))
+	item.get_node("Price").text = "$ %d" % char_data.get("stats", {}).get("price", 0)
 	
-	var skins = killer.get("skins", [])
+	var skins = char_data.get("skins", [])
 	if not skins.is_empty():
 		var thumb_path = skins[0].get("thumbnail", "")
 		if thumb_path != "" and ResourceLoader.exists(thumb_path):
@@ -35,8 +53,30 @@ func _create_killer_item(killer: Dictionary) -> Control:
 	
 	return item
 
-func select_item(item):
+func _create_emote_item(emote_name: String, emote_data: Dictionary) -> Control:
+	var item_scene = preload("res://UI/stuff/ShopButton.tscn")
+	var item = item_scene.instantiate()
+	
+	item.get_node("ItemName").text = emote_name
+	item.get_node("Price").text = "$ %d" % emote_data.get("Price", 0)
+	
+	if emote_data.get("Limited", false):
+		item.get_node("LimitedBadge").visible = true
+	
+	return item
+
+func select_char_item(item) -> void:
 	$InfoPanel.visible = true
 	$InfoPanel/CharName.text = item.get_node("ItemName").text
 	$InfoPanel/Price.text = item.get_node("Price").text
 	$InfoPanel/Render.texture = item.get_node("Render").texture
+
+func select_emote_item(item) -> void:
+	$InfoPanel.visible = true
+	$InfoPanel/CharName.text = item.get_node("ItemName").text
+	$InfoPanel/Price.text = item.get_node("Price").text
+	$InfoPanel/Render.texture = null
+
+func _clear_grid(grid: Node) -> void:
+	for child in grid.get_children():
+		child.queue_free()
