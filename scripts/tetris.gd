981d67f2898843731eb4efc6c5f1856c3d56a3d1
Virus01Official <virusidkoff@gmail.com>
2026-04-24 18:24:58 +0200

    more work on tetris

--- scripts/tetris.gd
+++ scripts/tetris.gd
@@ -6,17 +6,48 @@ const CELL_SIZE = 30
 var fall_timer: float = 0.0
 const FALL_SPEED: float = 0.5 
 
+var active_shape_name = ""
+var current_shape_instance = null
+
+var shapes = [
+	preload("res://UI/stuff/tetris/square.tscn"),
+]
+
+func _ready():
+	spawn_random_shape()
+
+func spawn_random_shape():
+	if current_shape_instance:
+		current_shape_instance.queue_free()
+
+	var chosen_scene = shapes[randi() % shapes.size()]
+	current_shape_instance = chosen_scene.instantiate()
+	add_child(current_shape_instance)
+
+	update_shape_position()
+
+func update_shape_position():
+	if current_shape_instance:
+		current_shape_instance.global_position = Vector2(
+			grid_x * CELL_SIZE,
+			grid_y * CELL_SIZE
+		)
+
 func _process(delta):
 	fall_timer += delta
-	if fall_timer >= FALL_SPEED and $".".visible:
+	if fall_timer >= FALL_SPEED and visible:
 		fall_timer = 0.0
-		move_down()
-	
-	position.x = grid_x * CELL_SIZE
-	position.y = grid_y * CELL_SIZE
-
-func move_left():
-	grid_x -= 1   
+		if current_shape_instance != null:
+			move_down()
 
 func move_down():
-	grid_y += 1  
+	grid_y += 1
+	update_shape_position()
+
+func move_left():
+	grid_x -= 1
+	update_shape_position()
+
+func move_right():
+	grid_x += 1
+	update_shape_position()
