465ee310befb0ba73bdd86a871eb2d6e32bc9734
Virus01Official <virusidkoff@gmail.com>
2026-05-23 12:53:15 +0200

    added Cursed Child skin and updated price for Sakuya; included Undertale image reference

--- scripts/originates.gd
+++ scripts/originates.gd
@@ -1,5 +1,6 @@
 extends Node
 
 var originates = {
-	"Touhou Project": "res://assets/images/touhou.png"
+	"Touhou Project": "res://assets/images/touhou.png",
+	"Undertale": "res://assets/images/undertale.png",
 }
